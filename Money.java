cad26cae0151d85500add700690f19bc55afc42f
victor <victor_77@naver.com>
2015-12-05 03:39:18 +0900

    refectoring to decoupling code that is in the testCode

--- Money.java
+++ Money.java
@@ -1,5 +1,15 @@
-public class Money{
+public abstract class Money{
 	protected int amount;
+
+	abstract Money times(int multiplier);
+	
+	static Money dollar(int amount){
+		return new Dollar(amount);
+	}
+
+	static Money franc(int amount){
+		return new Franc(amount);
+	}
 
 	public boolean equals(Object object){
 		Money money = (Money)object;
